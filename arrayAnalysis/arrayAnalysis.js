372aa41f13505334936cba49e331b25e47e8ecc1
zderApfel <scoutt172@yahoo.com>
2020-10-09 18:52:56 -0400

    Finished array analyzer

--- arrayAnalysis/arrayAnalysis.js
+++ arrayAnalysis/arrayAnalysis.js
@@ -1,3 +1,19 @@
-const analyze = (array) => {}
+const analyze = (array) => {
+    let object = {
+        average: average(array),
+        min: Math.min(...array),
+        max: Math.max(...array),
+        length: array.length
+    }
+
+    return object
+}
+
+function average(array){ //Rounded to nearest integer
+    let total = array.reduce((total, num) => {
+        return total + num;
+    })
+    return Math.round(total/array.length);
+}
 
 module.exports = analyze;
--- arrayAnalysis/arrayAnalysis.test.js
+++ arrayAnalysis/arrayAnalysis.test.js
@@ -1,1 +1,21 @@
-const analyze = require("./analyze");
+const analyze = require("./arrayAnalysis");
+
+test("Data for 4 numbers", () => {
+    expect(analyze([1,2,3,4])).toStrictEqual(
+        {
+            average: 3,
+            min: 1,
+            max: 4,
+            length: 4
+        })
+})
+
+test("Data for 11 numbers", () => {
+    expect(analyze([10,23,412,12,6,9,1,4,78,9,4])).toStrictEqual(
+        {
+            average: 52,
+            min: 1,
+            max: 412,
+            length: 11
+        })
+})
